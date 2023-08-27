b4905da80a28aa852c30de76e73f5878e1e7c329
Arda Demirel <yademirel21@gmail.com>
2023-08-27 22:56:21 +0300

    Added "Receive Payment" page

---
+++ app/src/main/java/com/mycompany/advioo/ui/fragments/ReceivePaymentFragment.kt
@@ -0,0 +1,39 @@
+package com.mycompany.advioo.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.mycompany.advioo.R
+import com.mycompany.advioo.databinding.FragmentReceivePaymentBinding
+
+class ReceivePaymentFragment : Fragment() {
+
+    private var _binding: FragmentReceivePaymentBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentReceivePaymentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupViews()
+    }
+
+    private fun setupViews(){
+        requireActivity().findViewById<BottomNavigationView>(R.id.bottomNavView).visibility = View.GONE
+    }
+}
